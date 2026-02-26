af3ae840b93e8a29867b5dd77f495c390072df99
Pierre Roux <pierre.roux@onera.fr>
2026-02-26 09:06:33 +0100

    Adapt to https://github.com/math-comp/math-comp/pull/1545

--- src/word.v
+++ src/word.v
@@ -27,6 +27,7 @@ From Coq Require Import Arith ZArith Lia.
 Require Import word_ssrZ.
 
 (* -------------------------------------------------------------------- *)
+Set SsrOldRewriteGoalsOrder.  (* change Set to Unset when porting the file, then remove the line when requiring MathComp >= 2.6 *)
 Set   Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
